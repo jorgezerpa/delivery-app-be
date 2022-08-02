45dd46701447186a96a58aa62d52590d81784bff
jorgezerpa <jorgezerpacoder@gmail.com>
2022-08-02 14:09:55 -0400

    install socket.io dependency. Prepare server to work with sockets

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
 const dotenv = require('dotenv');
 dotenv.config();
-const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
-const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
@@ -24,6 +26,6 @@ app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`App listen in port ${port}`);
 });
